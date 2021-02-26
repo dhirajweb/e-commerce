4d43733c297f5e7d3693363a88add134266a7239
dhiraj <dmakhijani1@gmail.com>
2021-02-26 04:16:18 -0800

    filter products

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -7,16 +8,37 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import './ProductList.css'
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+}));
 
 const ProductList = () => {
     const [productList, setProductList] = useState([])
-
-    const fetchData = () => {
+    const [categoryList, setCategoryList] = useState([])
+    const [filter, setFilter] = useState('all')
+    const classes = useStyles();
+    const fetchProducts = () => {
         fetch('https://fakestoreapi.com/products')
         .then(res => res.json())
         .then(result => {
             setProductList(result)
         })
+    }
+
+    const fetchCategories = () => {
+        fetch('https://fakestoreapi.com/products/categories')
+            .then(res=>res.json())
+            .then(res=>setCategoryList(res))
     }
 
     const titleTextStyle = {
@@ -39,16 +61,50 @@ const ProductList = () => {
         marginTop: '10px'
     }
 
+    const handleFilterChange = (filterVal) => {
+        setFilter(filterVal)
+        if(filterVal !== filter && filterVal !== 'all') {
+            fetch('https://fakestoreapi.com/products/category/'+filterVal)
+            .then(res=>res.json())
+            .then(result=>setProductList(result))
+        }
+    }
+
     useEffect(() => {
-        fetchData();
-    }, [])
+        if(filter === 'all') {
+            fetchProducts();
+            fetchCategories();
+        }
+    }, [filter])
 
     return (
         <div>
-            <div style={{display: 'flex'}}>
-                <Typography gutterBottom variant="subtitle1" style={{fontWeight: 'bold', margin: '20px 24px', display:'inline-block'}}>
+            <div>
+                <Typography variant="subtitle1" style={{fontWeight: 'bold', margin: '20px 24px', display:'inline-block',float:'left'}}>
                     {'Products('+productList.length+')'}
                 </Typography>
+                <div className='filterDiv' style={{display: 'inline-block', marginRight: '24px'}}>
+                    <Typography variant="subtitle2" style={{display: 'inline', marginRight: '10px', color: '#9c27b0'}}>
+                        Filter by category
+                    </Typography>
+                    <Select
+                        labelId="demo-simple-select-placeholder-label-label"
+                        id="demo-simple-select-placeholder-label"
+                        value={filter}
+                        onChange={(e) => handleFilterChange(e.target.value)}
+                        displayEmpty
+                        className={classes.selectEmpty}
+                        >
+                        <MenuItem value="all">
+                            All
+                        </MenuItem>
+                        {categoryList.map((category) => (
+                            <MenuItem key={category} value={category}>
+                                {category}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </div>
             </div>
             <Grid container spacing={3} style={{margin: '0', padding: '10px', width: '100%'}}>
             {productList.map((product) => (
